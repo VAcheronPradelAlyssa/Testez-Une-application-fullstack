31d8b53a346d626ce8b6190ed7f6da37efb92556
VAcheronPradelAlyssa <vacheron.alyssa@aol.com>
2025-05-26 21:57:57 +0200

    register spec

--- front/src/app/features/auth/components/register/register.component.spec.ts
+++ front/src/app/features/auth/components/register/register.component.spec.ts
@@ -1,6 +1,6 @@
 import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
@@ -9,6 +9,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { expect } from '@jest/globals';
 
 import { RegisterComponent } from './register.component';
+import { AuthService } from "../../services/auth.service";
+import { throwError } from "rxjs";
 
 describe('RegisterComponent', () => {
   let component: RegisterComponent;
@@ -20,14 +22,14 @@ describe('RegisterComponent', () => {
       imports: [
         BrowserAnimationsModule,
         HttpClientModule,
-        ReactiveFormsModule,  
+        ReactiveFormsModule,
         MatCardModule,
         MatFormFieldModule,
         MatIconModule,
         MatInputModule
-      ]
-    })
-      .compileComponents();
+      ],
+      providers: [AuthService]
+    }).compileComponents();
 
     fixture = TestBed.createComponent(RegisterComponent);
     component = fixture.componentInstance;
@@ -37,4 +39,36 @@ describe('RegisterComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('Should form value be of type RegisterRequest', () => {
+    const expectedEmail = 'test@test.com';
+    const expectedFirstName = 'firstName';
+    const expectedLastName = 'lastName';
+    const expectedPassword = 'password';
+
+    component.form.controls['email'].setValue(expectedEmail);
+    component.form.controls['firstName'].setValue(expectedFirstName);
+    component.form.controls['lastName'].setValue(expectedLastName);
+    component.form.controls['password'].setValue(expectedPassword);
+
+    component.submit();
+
+    const registerRequest = component.form.value;
+    expect(registerRequest).toEqual({
+      email: expectedEmail,
+      firstName: expectedFirstName,
+      lastName: expectedLastName,
+      password: expectedPassword
+    });
+  });
+
+  it('Should set onError to true when register fails', () => {
+    const authService = TestBed.inject(AuthService);
+    jest.spyOn(authService, 'register').mockReturnValue(throwError(() => 'Error'));
+
+    component.submit();
+
+    expect(component.onError).toBe(true);
+  });
+
 });
